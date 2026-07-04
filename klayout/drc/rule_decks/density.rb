b99297baec815312f0a05ab9433b20b34d4c0c4c
Leo Moser <leomoser99@gmail.com>
2026-07-04 16:16:25 +0200

    feat: check comp density, print density values
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- klayout/drc/rule_decks/density.rb
+++ klayout/drc/rule_decks/density.rb
@@ -22,10 +22,28 @@ GF180_DRC_REGISTRY.register(
 ) do
   chip_area = extent.sized(0.0).area
 
+  # Rule DCF.1b: Minimum global density for active layers (COMP+ Dummy COMP) shall be 25%.
+  logger.info('Executing rule DCF.1b')
+  comp_ratio = (comp + comp_dummy).area / chip_area
+  logger.info("COMP ratio: #{comp_ratio * 100} %")
+  if comp_ratio * 100 < 25
+    extent.output('DCF.1b',
+                  'DCF.1b : Global density for active layers (COMP+ Dummy COMP) shall be >= 25%')
+  end
+
+  # Rule DCF.1d: Maximum global density for active layers (COMP+ Dummy COMP) shall be 70%.
+  logger.info('Executing rule DCF.1d')
+  if comp_ratio * 100 > 70
+    extent.output('DCF.1d',
+                  'DCF.1d : Global density for active layers (COMP+ Dummy COMP) shall be <= 70%')
+  end
+
   # Rule PL.8: Poly2 coverage over the entire die shall be 14%.
   ## Dummy poly2 lines must be added to meet the minimum poly2 density requirement.
   logger.info('Executing rule PL.8')
-  if (poly2.area / chip_area) * 100 < 14
+  poly2_ratio = poly2.area / chip_area
+  logger.info("Poly2 ratio: #{poly2_ratio * 100} %")
+  if poly2_ratio * 100 < 14
     extent.output('PL.8',
                   'PL.8 : Poly2 coverage over the entire die shall be 14%.
                   Dummy poly2 lines must be added to meet the minimum poly2 density requirement. : 14%')
@@ -35,7 +53,9 @@ GF180_DRC_REGISTRY.register(
   ## (Refer to section 13.0 for Dummy Metal fill guidelines.
   ## Customer needs to ensure enough dummy metal to satisfy Metal1 coverage)
   logger.info('Executing rule M1.4')
-  if (metal1.area / chip_area) * 100 < 30
+  metal1_ratio = metal1.area / chip_area
+  logger.info("Metal1 ratio: #{metal1_ratio * 100} %")
+  if metal1_ratio * 100 < 30
     extent.output('M1.4',
                   'M1.4 : Metal1 coverage over the entire die shall be >30%
                   (Refer to section 13.0 for Dummy Metal fill guidelines.
@@ -46,7 +66,9 @@ GF180_DRC_REGISTRY.register(
   ## (Refer to section 13.0 for Dummy Metal fill guidelines.
   ## Customer needs to ensure enough dummy metal to satisfy Metal2 coverage)
   logger.info('Executing rule M2.4')
-  if (metal2.area / chip_area) * 100 < 30
+  metal2_ratio = metal2.area / chip_area
+  logger.info("Metal2 ratio: #{metal2_ratio * 100} %")
+  if metal2_ratio * 100 < 30
     extent.output('M2.4',
                   'M2.4 : Metal2 coverage over the entire die shall be >30%
                   (Refer to section 13.0 for Dummy Metal fill guidelines.
@@ -58,7 +80,9 @@ GF180_DRC_REGISTRY.register(
     ## (Refer to section 13.0 for Dummy Metal fill guidelines.
     ## Customer needs to ensure enough dummy metal to satisfy metal3 coverage)
     logger.info('Executing rule M3.4')
-    if (metal3.area / chip_area) * 100 < 30
+    metal3_ratio = metal3.area / chip_area
+    logger.info("Metal3 ratio: #{metal3_ratio * 100} %")
+    if metal3_ratio * 100 < 30
       extent.output('M3.4',
                     'M3.4 : metal3 coverage over the entire die shall be >30%
                       (Refer to section 13.0 for Dummy Metal fill guidelines.
@@ -71,7 +95,9 @@ GF180_DRC_REGISTRY.register(
     ## (Refer to section 13.0 for Dummy Metal fill guidelines.
     ## Customer needs to ensure enough dummy metal to satisfy metal4 coverage)
     logger.info('Executing rule M4.4')
-    if (metal4.area / chip_area) * 100 < 30
+    metal4_ratio = metal4.area / chip_area
+    logger.info("Metal4 ratio: #{metal4_ratio * 100} %")
+    if metal4_ratio * 100 < 30
       extent.output('M4.4',
                     'M4.4 : metal4 coverage over the entire die shall be >30%
                       (Refer to section 13.0 for Dummy Metal fill guidelines.
@@ -84,7 +110,9 @@ GF180_DRC_REGISTRY.register(
     ## (Refer to section 13.0 for Dummy Metal fill guidelines.
     ## Customer needs to ensure enough dummy metal to satisfy metal5 coverage)
     logger.info('Executing rule M5.4')
-    if (metal5.area / chip_area) * 100 < 30
+    metal5_ratio = metal5.area / chip_area
+    logger.info("Metal5 ratio: #{metal5_ratio * 100} %")
+    if metal5_ratio * 100 < 30
       extent.output('M5.4',
                     'M5.4 : metal5 coverage over the entire die shall be >30%
                       (Refer to section 13.0 for Dummy Metal fill guidelines.
@@ -97,7 +125,9 @@ GF180_DRC_REGISTRY.register(
     ## (Refer to section 13.0 for Dummy Metal-fill guidelines.
     ## Customer needs to ensure enough dummy metal to satisfy Metaln coverage).
     logger.info('Executing rule MT30.7')
-    if (top_metal.area / chip_area) * 100 < 30
+    top_metal_ratio = top_metal.area / chip_area
+    logger.info("MetalTop ratio: #{top_metal_ratio * 100} %")
+    if top_metal_ratio * 100 < 30
       extent.output('MT30.7',
                     'MT30.7 : Thick MetalTop coverage over the entire die shall be >30%
                                   (Refer to section 13.0 for Dummy Metal-fill guidelines.
@@ -108,7 +138,9 @@ GF180_DRC_REGISTRY.register(
     ## (Refer to section 10.3 for Dummy Metal-fill guidelines.
     ## Customer needs to ensure enough dummy metal to satisfy Metaln coverage)
     logger.info('Executing rule MT.3')
-    if (top_metal.area / chip_area) * 100 < 30
+    top_metal_ratio = top_metal.area / chip_area
+    logger.info("MetalTop ratio: #{top_metal_ratio * 100} %")
+    if top_metal_ratio * 100 < 30
       extent.output('MT.3',
                     'MT.3 : MetalTop coverage over the entire die shall be >30%
                                   (Refer to section 10.3 for Dummy Metal-fill guidelines.
